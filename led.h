d5aa4e63779332262d14a5c480c53b461beea4ba
JuneYoung76 <28446350+JuneYoung76@users.noreply.github.com>
2018-06-05 17:48:42 +0800

    Update led.h

--- led.h
+++ led.h
@@ -4,11 +4,9 @@
 #define SCLK 3
 #define RCLK 2
 #define LEDDATA 0
+#define DOTMASK 3
 
 typedef unsigned char uchar;
-
-//uchar led_pos[4]={0x01, 0x02, 0x04, 0x08};
-//uchar led_tab[10]={0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
 void led_gpio_init();
 void led_out(uchar data);
